32f6859f772e16d7e392dd710c713014b919c434
Hubert Dyrda <flovlie.pro@gmail.com>
2021-12-28 14:06:31 +0100

    adding new tasks and showing if a task done

--- js/script.js
+++ js/script.js
@@ -15,7 +15,9 @@
 
         for (const task of tasks) {
             htmlString += `
-              <li class="tasks__item">
+              <li 
+              class="tasks__item ${task.done ? "tasks__content--done" : ""}" 
+              >
                 ${task.content}
               </li>
             `;
@@ -24,8 +26,30 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
+    };
+
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
